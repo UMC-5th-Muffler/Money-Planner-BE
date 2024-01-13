8859cfa5790a6550bc3123dfb13cc07870ca7dca
shortboy7 <thdwldnd7@naver.com>
2024-01-13 21:04:49 +0900

    feat: 테스트 코드가 돌아가도록 수정
    Related to : #5

--- src/test/java/com/umc5th/muffler/domain/expense/service/ExpenseServiceTest.java
+++ src/test/java/com/umc5th/muffler/domain/expense/service/ExpenseServiceTest.java
@@ -9,9 +9,11 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpenseServiceTest {
     private final ExpenseService expenseService;
 
