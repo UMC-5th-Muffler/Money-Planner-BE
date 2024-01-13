a631b42493287bad708c0792e5baf3dbba506a49
shortboy7 <thdwldnd7@naver.com>
2024-01-13 16:15:29 +0900

    feat: category 생성 요청 DTO 추가, 검증용 annotation 위한 패키지 의존성 추가
    Related to : #1

---
+++ src/main/java/com/umc5th/muffler/domain/category/dto/NewCategoryRequest.java
@@ -0,0 +1,17 @@
+package com.umc5th.muffler.domain.category.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class NewCategoryRequest {
+    @NotNull
+    private Long memberId;
+    @NotBlank
+    private String categoryName;
+}
