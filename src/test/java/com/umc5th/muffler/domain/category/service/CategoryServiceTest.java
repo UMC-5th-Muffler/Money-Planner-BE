da2a86d08a4575157104d5115bf7f9fac0801442
shortboy7 <thdwldnd7@naver.com>
2024-01-14 17:04:56 +0900

    feat:Fixture 와 mock 객체를 도입하여 service 메서드 테스트 코드 수정.
    Related to : #5

--- src/test/java/com/umc5th/muffler/domain/category/service/CategoryServiceTest.java
+++ src/test/java/com/umc5th/muffler/domain/category/service/CategoryServiceTest.java
@@ -1,50 +1,74 @@
 package com.umc5th.muffler.domain.category.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
 
 import com.umc5th.muffler.domain.category.dto.CategoryDto;
 import com.umc5th.muffler.domain.category.dto.NewCategoryRequest;
+import com.umc5th.muffler.domain.category.repository.CategoryRepository;
+import com.umc5th.muffler.domain.member.repository.MemberRepository;
+import com.umc5th.muffler.entity.Category;
+import com.umc5th.muffler.entity.Member;
+
+import com.umc5th.muffler.fixture.CategoryFixture;
+import com.umc5th.muffler.fixture.MemberFixture;
 import com.umc5th.muffler.global.response.exception.CategoryException;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
-    private final CategoryService categoryService;
-
-    @Autowired
-    public CategoryServiceTest(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
-
+    @Mock
+    private CategoryRepository categoryRepository;
+    @Mock
+    private MemberRepository memberRepository;
+    @InjectMocks
+    private CategoryService categoryService;
     @Test
-    @Transactional
     public void 정상실행() throws CategoryException {
         //given
-        Long memberId = 1L;
-        NewCategoryRequest request = new NewCategoryRequest("카테고리0");
+        Member member = MemberFixture.MEMBER_ONE;
+        Category category = CategoryFixture.CATEGORY_ONE;
+        NewCategoryRequest request = new NewCategoryRequest(member.getId(),category.getName(), category.getIcon());
+
+        given(categoryRepository.findCategoryWithNameAndMemberId(request.getCategoryName(), request.getMemberId()))
+                .willReturn(Optional.empty());
+        given(memberRepository.findById(request.getMemberId()))
+                .willReturn(Optional.ofNullable(member));
+        given(memberRepository.save(any(Member.class))).willReturn(member);
+
         // when
-        CategoryDto newCategory = categoryService.createNewCategory(memberId, request);
+        CategoryDto newCategory = categoryService.createNewCategory(1L, request);
         //then
-        assertEquals(newCategory.getName(), request.getCategoryName());
+        assertEquals(request.getCategoryName(), newCategory.getName());
     }
 
     @Test
-    @Transactional
     public void 중복_이름() throws CategoryException {
         // given
-        Long memberId = 1L;
-        NewCategoryRequest request = new NewCategoryRequest("카테고리");
+        Member member = MemberFixture.MEMBER_ONE;
+        Category haveCategory = CategoryFixture.CATEGORY_ONE;
+        member.addCategory(haveCategory);
+
+        NewCategoryRequest request = new NewCategoryRequest(member.getId(),haveCategory.getName(), haveCategory.getIcon());
         // when
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(categoryRepository.findCategoryWithNameAndMemberId(request.getCategoryName(), request.getMemberId()))
+                .willReturn(Optional.of(haveCategory));
 
         // then
         Assertions.assertThrows(CategoryException.class, () -> {
-            CategoryDto newCategory = categoryService.createNewCategory(memberId, request);
+            categoryService.createNewCategory(member.getId(), request);
         });
     }
 
@@ -52,13 +76,14 @@ class CategoryServiceTest {
     @Transactional
     public void 사용자_아이디가_없는경우() throws CategoryException {
         //given
-        Long memberId = 1000L;
-        NewCategoryRequest request = new NewCategoryRequest("카테고리1");
+        Member member = MemberFixture.MEMBER_TWO;
+        Category category = CategoryFixture.CATEGORY_ONE;
+        NewCategoryRequest request = new NewCategoryRequest(member.getId(), category.getName(), category.getIcon());
+
+        given(memberRepository.findById(member.getId())).willReturn(Optional.empty());
         // when
         Assertions.assertThrows(CategoryException.class, () -> {
-            CategoryDto newCategory = categoryService.createNewCategory(memberId, request);
+           categoryService.createNewCategory(request.getMemberId(), request);
         });
-        //then
     }
-
 }
