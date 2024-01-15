657b6c4727ff935d25c12d9d4f0aa414db05c38a
hajungIm <ihj_ewha@ewhain.net>
2024-01-15 18:08:12 +0900

    refactor : GoalConverter 사용
    Related to: #4

--- src/main/java/com/umc5th/muffler/domain/goal/controller/GoalController.java
+++ src/main/java/com/umc5th/muffler/domain/goal/controller/GoalController.java
@@ -1,15 +1,14 @@
 package com.umc5th.muffler.domain.goal.controller;
 
+import com.umc5th.muffler.domain.goal.dto.GoalConverter;
 import com.umc5th.muffler.domain.goal.dto.GoalCreateRequest;
 import com.umc5th.muffler.domain.goal.dto.GoalPreviousResponse;
-import com.umc5th.muffler.domain.goal.dto.GoalTerm;
 import com.umc5th.muffler.domain.goal.service.GoalCreateService;
 import com.umc5th.muffler.domain.goal.service.GoalService;
 import com.umc5th.muffler.entity.Goal;
 import com.umc5th.muffler.global.response.Response;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,13 +36,7 @@ public class GoalController {
     @GetMapping("/previous")
     public Response<GoalPreviousResponse> getPrevious(@RequestParam Long memberId) {
         List<Goal> goals = goalService.getGoals(memberId);
-        GoalPreviousResponse response = new GoalPreviousResponse(
-                goals.stream()
-                        .sorted(Comparator.comparing(Goal::getStartDate).reversed())
-                        .map(goal -> new GoalTerm(goal.getStartDate(), goal.getEndDate()))
-                        .collect(Collectors.toList())
-        );
-        return Response.success(response);
+        return Response.success(GoalConverter.getGoalPreviousResponse(goals));
     }
 
     @DeleteMapping("/{goalId}")
--- src/test/java/com/umc5th/muffler/fixture/GoalFixture.java
+++ src/test/java/com/umc5th/muffler/fixture/GoalFixture.java
@@ -3,14 +3,14 @@ package com.umc5th.muffler.fixture;
 import com.umc5th.muffler.entity.Goal;
 import java.time.LocalDate;
 
-public class GoalEntityFixture {
+public class GoalFixture {
     public static Goal create() {
         return Goal.builder()
                 .id(1L)
                 .startDate(LocalDate.of(2024, 1, 1))
                 .endDate(LocalDate.of(2024, 1, 2))
                 .title("title")
-                .detail("detail")
+                .memo("memo")
                 .icon("icon")
                 .totalBudget(10000L)
                 .build();
