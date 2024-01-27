664d7da03d708dab092901f436bcb0ba5d07d3cb
Sj0-0i <sjy140801@dgu.ac.kr>
2024-01-28 02:16:53 +0900

    test: 소비 검색 controller test Related to: #25

--- src/main/java/com/umc5th/muffler/domain/expense/controller/ExpenseController.java
+++ src/main/java/com/umc5th/muffler/domain/expense/controller/ExpenseController.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Positive;
 import com.umc5th.muffler.domain.expense.dto.DailyExpenseDetailsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,7 @@ import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/expense")
 public class ExpenseController {
     private final ExpenseService expenseService;
