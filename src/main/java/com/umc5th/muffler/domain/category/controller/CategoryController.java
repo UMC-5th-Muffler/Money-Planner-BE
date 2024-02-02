80323ca00817905d03b4949ad00c47e6cf4b002f
shortboy7 <thdwldnd7@naver.com>
2024-02-03 08:52:34 +0900

    docs : 카테고리 swagger 세부 주석 추가
    Related to : #1

--- src/main/java/com/umc5th/muffler/domain/category/controller/CategoryController.java
+++ src/main/java/com/umc5th/muffler/domain/category/controller/CategoryController.java
@@ -8,9 +8,18 @@ import com.umc5th.muffler.domain.category.dto.UpdateCategoryNameIconRequest;
 import com.umc5th.muffler.domain.category.dto.UpdateCategoryPriorityVisibilityRequest;
 import com.umc5th.muffler.domain.category.service.CategoryService;
 import com.umc5th.muffler.global.response.Response;
+import com.umc5th.muffler.global.response.code.ErrorCode;
+import com.umc5th.muffler.global.swagger.ErrorResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.security.Principal;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/category")
+@Validated
+@Tag(name = "Category", description = "카테고리 API")
 public class CategoryController {
     private final CategoryService categoryService;
 
@@ -33,10 +44,18 @@ public class CategoryController {
     }
 
     @PatchMapping("/single")
+    @Operation(summary = "Category icon/name update",
+            description = "특정 카테고리의 아이콘이나 이름을 업데이트 한다. 가지고 있는 카테고리 중 중복되는 이름은 허용되지 않고, "
+                    + "기본으로 가지고 있는 카테고리의 아이콘은 변경할 수 없다. 카테고리 아이콘/이름 수정 화면에서 저장버튼 누를 시 호출")
+    @ErrorResponses(value = {
+            ErrorCode.MEMBER_NOT_FOUND, ErrorCode.CATEGORY_NOT_FOUND, ErrorCode.ACCESS_TO_OTHER_USER_CATEGORY,
+            ErrorCode.CANNOT_UPDATE_ETC_CATEGORY_NAME, ErrorCode.DUPLICATED_CATEGORY_NAME, ErrorCode.CANNOT_UPDATE_DEFAULT_ICON
+    })
     public Response<Void> updateCategoryNameOrIcon(Principal principal, @RequestBody @Valid UpdateCategoryNameIconRequest request) {
         categoryService.updateNameOrIcon(principal.getName(), request);
         return Response.success();
     }
+
     @PatchMapping
     public Response<Void> updateCategoryPriorityVisibility(Principal principal, @RequestBody @Valid UpdateCategoryPriorityVisibilityRequest request) {
         categoryService.updateBatchPriorityOrVisibility(principal.getName(), request);
