664ab9bd68060fcc1fada7b644e7a6ba51d913cf
shortboy7 <thdwldnd7@naver.com>
2024-02-02 16:40:53 +0900

    refactor : 카테고리 순서 api deserialize 문제 해결
    Related to : #1

--- src/main/java/com/umc5th/muffler/domain/category/dto/UpdateCategoryPriorityVisibilityRequest.java
+++ src/main/java/com/umc5th/muffler/domain/category/dto/UpdateCategoryPriorityVisibilityRequest.java
@@ -4,9 +4,11 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UpdateCategoryPriorityVisibilityRequest {
     private List<CategoryPriorityVisibilityDTO> categories;
