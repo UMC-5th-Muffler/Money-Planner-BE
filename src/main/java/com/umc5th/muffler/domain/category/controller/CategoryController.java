994238bd84a7e0dfea222af5c64155995e99da25
shortboy7 <thdwldnd7@naver.com>
2024-02-03 18:12:49 +0900

    docs : 카테고리 개요 리스트를 조회하는 api에 대한 자세한 설명 추가
    Related to : #1

--- src/main/java/com/umc5th/muffler/domain/category/controller/CategoryController.java
+++ src/main/java/com/umc5th/muffler/domain/category/controller/CategoryController.java
@@ -10,7 +10,6 @@ import com.umc5th.muffler.domain.category.dto.UpdateCategoryNameIconRequest;
 import com.umc5th.muffler.domain.category.dto.UpdateCategoryPriorityVisibilityRequest;
 import com.umc5th.muffler.domain.category.service.CategoryService;
 import com.umc5th.muffler.global.response.Response;
-import com.umc5th.muffler.global.response.code.ErrorCode;
 import com.umc5th.muffler.global.swagger.ErrorResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -82,7 +81,7 @@ public class CategoryController {
 
     @GetMapping("/all")
     @Operation(summary = "숨긴 카테고리 포함 전체 카테고리 조회 API", description = "숨긴 카테고리를 포함한 전체 카테고리를 조회하는 API, "
-            + "priority를 오름차순으로 정렬하여 가지고 있는 모든 카테고리를 리스트 형식으로 반환한다.")
+            + "priority를 오름차순으로 정렬하여 가지고 있는 모든 카테고리를 리스트 형식으로 반환한다. 카테고리 편집화면에 진입할 때 사용")
     @ErrorResponses(value = {MEMBER_NOT_FOUND})
     public Response<GetCategoryListResponse> getCategoryList(Principal principal) {
         GetCategoryListResponse response = this.categoryService.getActiveCategories(principal.getName());
@@ -90,6 +89,10 @@ public class CategoryController {
     }
     
     @GetMapping("/visible")
+    @Operation(summary = "숨김 처리 하지 않은 카테고리의 이름 아이콘 조회 API",
+            description = "카테고리 선택 뷰에 필요한 카테고리 아이콘, 이름, 아이디 조회 API, "
+                    + "우선순위 별로 정렬하여 제공한다. 홈 화면, 소비 등록, 목표 등록, 반복 기록 등록에서 사용")
+    @ErrorResponses(value = {MEMBER_NOT_FOUND})
     public Response<GetCategoryListResponse> getVisibleOutlineCategoryList(Principal principal) {
         GetCategoryListResponse response = this.categoryService.getVisibleOutlineCategories(principal.getName());
         return Response.success(response);
