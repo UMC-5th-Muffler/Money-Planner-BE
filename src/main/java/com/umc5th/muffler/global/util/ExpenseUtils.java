7c840cf9f2f5cb58a079737184991c4a9a10ed32
hajungIm <ihj_ewha@ewhain.net>
2024-02-03 20:53:12 +0900

    fix: 유틸리티 클래스 인스턴스화 방지

--- src/main/java/com/umc5th/muffler/global/util/ExpenseUtils.java
+++ src/main/java/com/umc5th/muffler/global/util/ExpenseUtils.java
@@ -4,6 +4,8 @@ import com.umc5th.muffler.entity.Expense;
 import java.util.List;
 
 public class ExpenseUtils {
+    private ExpenseUtils() {}
+
     public static Long sumExpenseCosts(List<Expense> expenses) {
         return expenses.stream()
                 .mapToLong(Expense::getCost)
