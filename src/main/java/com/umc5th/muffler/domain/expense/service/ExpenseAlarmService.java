39cec3a8682a64c09cb0ba1afa7edc3b6e19df0a
hajungIm <ihj_ewha@ewhain.net>
2024-02-04 16:33:47 +0900

    rename: ExpenseAlarm 관련 이름 간단하게 변경

--- src/main/java/com/umc5th/muffler/domain/expense/service/ExpenseAlarmService.java
+++ src/main/java/com/umc5th/muffler/domain/expense/service/ExpenseAlarmService.java
@@ -1,10 +1,10 @@
 package com.umc5th.muffler.domain.expense.service;
 
-import static com.umc5th.muffler.entity.constant.ExpenseAlarm.CATEGORY;
-import static com.umc5th.muffler.entity.constant.ExpenseAlarm.DAILY;
-import static com.umc5th.muffler.entity.constant.ExpenseAlarm.TOTAL;
+import static com.umc5th.muffler.entity.constant.ExpenseAlarmTitle.CATEGORY;
+import static com.umc5th.muffler.entity.constant.ExpenseAlarmTitle.DAILY;
+import static com.umc5th.muffler.entity.constant.ExpenseAlarmTitle.TOTAL;
 
-import com.umc5th.muffler.domain.expense.dto.AlarmControlDTO;
+import com.umc5th.muffler.domain.expense.dto.ExpenseAlarm;
 import com.umc5th.muffler.domain.expense.repository.ExpenseRepository;
 import com.umc5th.muffler.domain.goal.repository.CategoryGoalRepository;
 import com.umc5th.muffler.entity.Category;
@@ -24,47 +24,47 @@ public class ExpenseAlarmService {
     private final CategoryGoalRepository categoryGoalRepository;
     private final ExpenseRepository expenseRepository;
 
-    public List<AlarmControlDTO> getAlarms(DailyPlan dailyPlan, Category category, Long expenditure) {
-        List<AlarmControlDTO> alarms = new ArrayList<>();
+    public List<ExpenseAlarm> getAlarms(DailyPlan dailyPlan, Category category, Long expenditure) {
+        List<ExpenseAlarm> alarms = new ArrayList<>();
         setDailyAlarm(dailyPlan, expenditure, alarms);
         setCategoryAlarm(category, dailyPlan.getGoal(), expenditure, alarms);
         setGoalAlarm(dailyPlan.getGoal(), expenditure, alarms);
         return alarms;
     }
 
-    private void setDailyAlarm(DailyPlan dailyPlan, Long expenditure, List<AlarmControlDTO> alarms) {
+    private void setDailyAlarm(DailyPlan dailyPlan, Long expenditure, List<ExpenseAlarm> alarms) {
         if (dailyPlan.isPossibleToAlarm(expenditure)) {
-            alarms.add(new AlarmControlDTO(
+            alarms.add(new ExpenseAlarm(
                     DAILY, dailyPlan.getBudget(),
                     dailyPlan.getTotalCost() + expenditure - dailyPlan.getBudget()));
         }
     }
 
-    private void setCategoryAlarm(Category category, Goal goal, Long expenditure, List<AlarmControlDTO> alarms) {
+    private void setCategoryAlarm(Category category, Goal goal, Long expenditure, List<ExpenseAlarm> alarms) {
         categoryGoalRepository.findByGoalIdAndCategoryId(goal.getId(), category.getId())
                 .ifPresent(categoryGoal -> {
                     Long totalCategoryExpense = expenseRepository.sumCategoryExpenseWithinGoal(goal.getMember().getId(), category, goal);
 
                     if (categoryGoal.isPossibleToAlarm(totalCategoryExpense, expenditure)) {
-                        alarms.add(new AlarmControlDTO(
+                        alarms.add(new ExpenseAlarm(
                                 CATEGORY, categoryGoal.getBudget(),
                                 totalCategoryExpense + expenditure - categoryGoal.getBudget()));
                     }
                 });
     }
 
-    private void setGoalAlarm(Goal goal, Long expenditure, List<AlarmControlDTO> alarms) {
+    private void setGoalAlarm(Goal goal, Long expenditure, List<ExpenseAlarm> alarms) {
         Long totalGoalExpense = expenseRepository.sumCostByMemberAndDateBetween(goal.getMember().getId(), goal.getStartDate(), goal.getEndDate());
 
         if (goal.isPossibleToAlarm(totalGoalExpense, expenditure)) {
-            alarms.add(new AlarmControlDTO(
+            alarms.add(new ExpenseAlarm(
                     TOTAL, goal.getTotalBudget(),
                     totalGoalExpense + expenditure - goal.getTotalBudget()));
         }
     }
 
-    public List<AlarmControlDTO> getDailyAlarm(DailyPlan dailyPlan, Long expenditure) {
-        List<AlarmControlDTO> alarm = new ArrayList<>();
+    public List<ExpenseAlarm> getDailyAlarm(DailyPlan dailyPlan, Long expenditure) {
+        List<ExpenseAlarm> alarm = new ArrayList<>();
         setDailyAlarm(dailyPlan, expenditure, alarm);
         return alarm;
     }
