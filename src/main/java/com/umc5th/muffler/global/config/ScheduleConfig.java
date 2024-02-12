b3522958d4b741a493dab81b71a36fe41be396d0
shortboy7 <thdwldnd7@naver.com>
2024-02-12 16:45:15 +0900

    feat : 동시에 실행된 스케쥴 서비스를 처리하기 위한 thread pool size 설정 추가
    Related to : #17

---
+++ src/main/java/com/umc5th/muffler/global/config/ScheduleConfig.java
@@ -0,0 +1,24 @@
+package com.umc5th.muffler.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer {
+    @Value("${thread.pool.size")
+    private int POOL_SIZE;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+
+        scheduler.setPoolSize(POOL_SIZE);
+        scheduler.setThreadNamePrefix("스케쥴러 스레드-");
+        scheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(scheduler);
+    }
+}
