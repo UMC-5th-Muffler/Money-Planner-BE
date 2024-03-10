615286a0ee2a6c2ad0ccb8074b827e8f0bc2aff9
shortboy7 <thdwldnd7@naver.com>
2024-03-10 17:04:35 +0900

    feat : swagger에서 https 요청을 보낼 수 있도록 함

--- src/main/java/com/umc5th/muffler/MufflerApplication.java
+++ src/main/java/com/umc5th/muffler/MufflerApplication.java
@@ -1,11 +1,19 @@
 package com.umc5th.muffler;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url="https://muffler.world", description = "Deploy Server url"),
+				@Server(url="http://localhost:8080", description = "Local Server url")
+		}
+)
 public class MufflerApplication {
 
 	public static void main(String[] args) {
