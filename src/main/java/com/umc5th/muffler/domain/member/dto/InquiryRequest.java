ccbc53ba3ef6da9ed7c0cfc04812fc9291a1bad8
heejjinkim <06.hjhj.12@gmail.com>
2024-03-14 20:34:33 +0900

    feat: email 유효성 검사 추가
    Related to: #43

--- src/main/java/com/umc5th/muffler/domain/member/dto/InquiryRequest.java
+++ src/main/java/com/umc5th/muffler/domain/member/dto/InquiryRequest.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class InquiryRequest {
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$", message = "Invalid email address")
     private String email;
     @NotBlank
     private String content;
